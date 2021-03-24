b642b195e8a02663f119b6c724710c4602332217
damian-sketch <cedricdamian19@gmail.com>
2021-03-24 09:30:43 +0300

    Modify 'About Me' section

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -8,7 +8,8 @@ function About(){
     <div className="About">  
     <div className="aboutText">
       <h1>ABOUT ME</h1>
-      <p>Talented full-stack software engineer passionate about Frontend Engineering, UI/UX and backend engineering and in directing development with a creative and performance-oriented approach. Skilled in deployment using AWS and fluent in the MERN stack. Well-organized and customer-focused with proven skills in project management and team leadership.
+      <p>Innovative Software Developer with a strong liking for Frontend Engineering. I have a technical, hands-on mindset and I believe in conclusive deep dives and solving problems at the root.
+        With relevant industry experience of 2+ years, I have sharpened my Communication and Team Management skills that enable me to thrive in a remote working environment.
         All of my projects, including this site can be found on <a href="https://github.com/damian-sketch?tab=repositories">Github</a>
       </p>
      </div> 
--- src/components/experience/index.js
+++ src/components/experience/index.js
@@ -11,10 +11,10 @@ function Experience(){
       <h1>MY EXPERIENCE</h1>
       <p>
       <h4>ANDELA, KENYA(OCT '18 - OCT '19)</h4>
- At Andela, I wrote solid code in React and Redux for software systems that run the internal functions of the company including day-to-day activities such as access control and asset management.
+ At Andela, I wrote solid code in React and Redux for software systems that run the internal functions of the company such as access control. I also got the chance to design and develop API backends for some of their internal products in Python.
  
       <h4>QA Engineer, Trilogy(AUG '20 - PRESENT)</h4>
-      At Trilogy, I am part of the QA Automation team which focuses on automating End To End Tests using high-quality company technology which runs on Java.
+      At Trilogy, I am part of the QA Automation team which focuses on automating E2Es from over 80 products in Crossover's portfolio through Behave, Gherkin, Selenium and Python.
       </p>
      </div> 
     </div> 
