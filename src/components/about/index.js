80e546409ee45a30182bedf08159557f0b112644
damian-sketch <cedricdamian19@gmail.com>
2021-10-06 19:16:27 +0300

    import useState

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,9 +1,8 @@
-import React, { useEffect} from 'react';
+import React, { useEffect, useState} from 'react';
 import Aos from "aos";
 import "aos/dist/aos.css";
 import './styles.css';
 import { format } from 'date-fns'
-import { useState } from 'react/cjs/react.development';
 
 
 const About = () => {
