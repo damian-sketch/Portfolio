8d2c49d76fd37e3ad934d40cd0bd77c361e02bed
damian-sketch <cedricdamian19@gmail.com>
2022-09-12 18:53:07 +0300

    Update resume blobs

--- src/components/experience/index.js
+++ src/components/experience/index.js
@@ -14,20 +14,21 @@ function Experience() {
           </h4>
           <ul className="exp-list">
             <li>
-              - I manually executed functional testing of over 100 test
-              scenarios beating team averages by 20% against work completion
-              deadlines.
+              - Delivered releases with reduced defects by 80% in 6 months by
+              regularly performing smoke and regression testing.
             </li>
             <br></br>
             <br></br>
             <li>
-              - Automated 200+ End-to-End, API, and Mobile tests using the
-              internal Gherkin/behave engine
+              - Implemented quality control standards by developing a JIRA
+              workflow to ensure consistent approach and results in our QA team.
             </li>
             <br></br>
             <br></br>
             <li>
-              - Reduced false-positive defect rate from 32% to less than 5%
+              - Developed 50+ test scenarios for a learning management system
+              using Python and Cucumber framework to cover all end to end
+              scenarios which put test coverage of the application at 85%.
             </li>
           </ul>
           <h4>
@@ -36,14 +37,15 @@ function Experience() {
           </h4>
           <ul className="exp-list">
             <li>
-              - Developed a test framework using Selenium and TypeScript for a
-              web3 wallet.
+              - Developed a Selenium based automation framework using TypeScript
+              to cover high-level functionalities of a web3 wallet extension.
             </li>
             <br></br>
             <br></br>
             <li>
-              - Setup a GraphQL testing framework for a hotel delivery
-              application
+              - Performed manual and automated testing of API endpoints for a
+              food delivery application using GraphQL queries which helped
+              optimize response speeds.
             </li>
             <br></br>
             <br></br>
@@ -59,25 +61,30 @@ function Experience() {
           </h4>
           <ul className="exp-list">
             <li>
-              - Automated 20 test cases using TypeScript as the primary language
+              - Automated end-to-end test cases to cover the high-level
+              functionalities for our core fintech platform, increasing our test
+              coverage by 30%
             </li>
             <br></br>
             <br></br>
             <li>
-              - Streamlined automation capabilities that improved test life
-              cycle by 80%
+              - Converted the framework to use Cucumber’s BDD technique by
+              implementing the cucumber pre-processor therefore making test
+              cases coherent to non-development members of the company.
             </li>
             <br></br>
             <br></br>
             <li>
-              - Converted more than 50 test cases from the old Cypress framework
-              to a new Cucumber based framework
+              - Maintained the CI/CD test pipeline continuously using Gitlab and
+              Cypress dashboard to identify and fix flaky tests. This has
+              enabled us to achieve 100% success rate on our test pipeline.
             </li>
             <br></br>
             <br></br>
             <li>
-              - Achieved 100% success rate on tests by continuously maintaining
-              the CI/CD pipeline
+              - Executed manual testing on our core fintech platforms to ensure
+              releases are deployed without a hitch(smoke, API, sanity,
+              regression testing)
             </li>
           </ul>
         </div>
@@ -92,7 +99,7 @@ function Experience() {
           <ul className="exp-list">
             <li>
               - I am an active open-source contributor to the
-              <a href="https://github.com/Christmas-4-Kids/">
+              <a href="https://github.com/Christmas-4-Kids/c4k/pull/59/">
                 {" "}
                 Christmas 4 Kids{" "}
               </a>
@@ -102,11 +109,13 @@ function Experience() {
             <br></br>
             <li>
               - I updated the UI/UX to be accessible to all users utilizing a11y
-              standards
+              standards using React Native
             </li>
             <br></br>
             <br></br>
-            <li>- I updated the colors and the overall look and feel</li>
+            <li>
+              - I updated the colors and the overall look and feel using CSS
+            </li>
           </ul>
         </div>
       </div>
