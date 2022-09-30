2ba4d15c8ec3f66e389c2fac868468fdbca2b5d0
damian-sketch <cedricdamian19@gmail.com>
2022-09-30 15:57:22 +0300

    Change availability status

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import './styles.css';
-function Contact(){
-    
-    const Redirect = () => {
-        window.open('mailto:cedricdamian19@gmail.com')
-         }
-    return(
-
+import React from "react";
+import "./styles.css";
+function Contact() {
+  const Redirect = () => {
+    window.open("mailto:cedricdamian19@gmail.com");
+  };
+  return (
     <div className="contact">
-        <div data-aos="fade-down-left" className="contact-text">
-          <h1>Get In Touch</h1>
-          <p>I am open to any Frontend Engineering roles that will pose an exciting challenge to me and my career.</p>
-          <button onClick={Redirect}>Send Mail</button>
-        </div>
+      <div data-aos="fade-down-left" className="contact-text">
+        <h1>Get In Touch</h1>
+        <p>
+          Want to Pair Program? I'm open to open-source contributing too. Hit me
+          up!
+        </p>
+        <button onClick={Redirect}>Send Mail</button>
+      </div>
     </div>
-    )
+  );
+}
 
-} 
-
-export default Contact
+export default Contact;
